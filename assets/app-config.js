51fa0225e8211cfdefaab6a65571cc2060c46f32
bigtk1999 <109627551+bigtk1999@users.noreply.github.com>
2025-08-12 11:02:21 +0700

    B

---
+++ assets/app-config.js
@@ -0,0 +1,7 @@
+// assets/app-config.js
+export const BANK = {
+  bankCode: 'SCB',
+  accountNumber: '32974522222',
+  accountName: 'NGO TUAN ANH',
+  template: 'compact',
+};
--- assets/firebase-config.js
+++ assets/firebase-config.js
@@ -1,13 +1,11 @@
-
 // assets/firebase-config.js
-// Public web config; safe to keep on client.
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getFunctions } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-functions.js";
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyB-A0omawXnKjVdq_4ea3UWjJhAEpx-tSg",
+const firebaseConfig = {
+  apiKey: "AIzaSyB-A0omawXnKJdq_4ea3UWjJhAEpx-tSg",
   authDomain: "tuananh-3b562.firebaseapp.com",
   projectId: "tuananh-3b562",
   storageBucket: "tuananh-3b562.firebasestorage.app",
@@ -17,6 +15,6 @@ export const firebaseConfig = {
 };
 
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 export const auth = getAuth(app);
-export const storage = getStorage(app);
+export const db = getFirestore(app);
+export const functions = getFunctions(app, 'asia-southeast1');
